dc5cee93ceab67b8c05893deb572c67c4698fb60
joshua1988 <jangkeehyo@gmail.com>
2019-06-13 23:50:40 +0900

    컴포넌트를 분리한 경우와 분리하지 않은 경우 2가지로 예시 코드 작성

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,39 @@
 <template>
   <div id="app">
-    <checkbox @change="onChange" :list="codeList" v-model="selectedList" :include-all="true"></checkbox>
+    <input type="checkbox" id="all" @change="checkAll">
+    <label for="all">All</label>
+    <input type="checkbox" id="box1" v-model="check1">
+    <label for="box1">code1</label>
+    <input type="checkbox" id="box2" v-model="check2">
+    <label for="box2">code2</label>
+    <input type="checkbox" id="box3" v-model="check3">
+    <label for="box3">code3</label>
   </div>
 </template>
 
 <script>
-import Checkbox from './components/Checkbox'
-
+// NOTE: 컴포넌트로 분리하지 않은 경우
 export default {
-  name: 'app',
-  components: {
-    Checkbox
-  },
   data() {
     return {
-      codeList: [
-        {code: 'code1', desc: 'option1'},
-        {code: 'code2', desc: 'option2'},
-        {code: 'code3', desc: 'option3'}
-      ],
-      selectedList: []
+      all: null,
+      check1: null,
+      check2: null,
+      check3: null,
+    }
+  },
+  watch: {
+    check3(value) {
+      if (value) {
+        this.check1 = true;
+      }
     }
   },
   methods: {
-    onChange(val) {
-      if (val.includes('code3')) {
-        let arr = this.selectedList.filter(function(val) {return val !== 'code1'});
-        arr.push('code1');
-        this.selectedList = arr;
-      }
+    checkAll() {
+      this.check1 = event.target.checked;
+      this.check2 = event.target.checked;
+      this.check3 = event.target.checked;
     }
   }
 }
---
+++ src/App2.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <check-box label="all" v-model="all"></check-box>
+    <check-box label="check1" v-model="check1"></check-box>
+    <check-box label="check2" v-model="check2"></check-box>
+    <check-box label="check3" v-model="check3"></check-box>
+  </div>
+</template>
+
+<script>
+// NOTE: 개별 컴포넌트로 분리한 경우
+import CheckBox from './components/CheckBox.vue';
+
+export default {
+  components: {
+    CheckBox
+  },
+  data() {
+    return {
+      all: false,
+      check1: false,
+      check2: false,
+      check3: false,
+    }
+  },
+  watch: {
+    all(value) {
+      this.check1 = value;
+      this.check2 = value;
+      this.check3 = value;
+    },
+    check3(value) {
+      this.check1 = value;
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
